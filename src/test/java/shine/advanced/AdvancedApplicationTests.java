22066ea635502bc63e05e1fc08e3a4d8221f1eaf
jiwoo <zbqmgldjfh@gmail.com>
2022-08-03 14:47:04 +0900

    feat: 로그 추적기 v0

---
+++ src/test/java/shine/advanced/AdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package shine.advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
