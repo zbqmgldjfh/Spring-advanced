2bb6375bcab18a495e14b801093af41ba1211884
jiwoo <zbqmgldjfh@gmail.com>
2022-08-04 09:21:58 +0900

    feat(HelloTraceV2): 로거 V2 버전 적용

---
+++ src/main/java/shine/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,31 @@
+package shine.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import shine.advanced.trace.TraceStatus;
+import shine.advanced.trace.hellotrace.HelloTraceV2;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(@RequestParam String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
---
+++ src/main/java/shine/advanced/app/v2/OrderServiceV2.java
@@ -0,0 +1,29 @@
+package shine.advanced.app.v2;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import shine.advanced.trace.TraceId;
+import shine.advanced.trace.TraceStatus;
+import shine.advanced.trace.hellotrace.HelloTraceV2;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV2 {
+
+    private final OrderRepositoryV2 orderRepository;
+    private final HelloTraceV2 trace;
+
+
+    public void orderItem(TraceId traceId, String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.beginSync(traceId, "OrderService.orderItem()");
+            orderRepository.save(status.getTraceId(), itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
