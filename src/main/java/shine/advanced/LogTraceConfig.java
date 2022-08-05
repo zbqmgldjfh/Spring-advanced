fffe90272935335828e42887262bacc703b97f1d
jiwoo <zbqmgldjfh@gmail.com>
2022-08-05 13:34:10 +0900

    feat(ThreadLocalLogTrace): ThreadLocal을 사용하도록 개발 완료

--- src/main/java/shine/advanced/LogTraceConfig.java
+++ src/main/java/shine/advanced/LogTraceConfig.java
@@ -2,14 +2,14 @@ package shine.advanced;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import shine.advanced.trace.logtrace.FieldLogTrace;
 import shine.advanced.trace.logtrace.LogTrace;
+import shine.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
---
+++ src/test/java/shine/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -0,0 +1,25 @@
+package shine.advanced.trace.logtrace;
+
+import org.junit.jupiter.api.Test;
+import shine.advanced.trace.TraceStatus;
+
+class ThreadLocalLogTraceTest {
+
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
+
+    @Test
+    public void begin_end_level2() {
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+        TraceStatus status1 = trace.begin("hello");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+}
